c842ac4565340fdc422ba5d3f89eb695fce99dcd
Nathan karns <107375013+Karns11@users.noreply.github.com>
2023-04-24 18:30:43 -0400

    Update DateInput.js

--- src/components/DateInput.js
+++ src/components/DateInput.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './DateInput.css';
 
-function DateInput({ dateInput, setDateInput, handleDateReset }) {
+function DateInput({ StartDateInput, setStartDateInput, handleStartDateReset, endDateInput, setEndDateInput, handleEndDateReset }) {
   return (
     <div className='column'>
       <div className="input-group px-5 mb-0">
-        <span className="input-group-text" >Start Date</span>
-        <input type="text" className="form-control" value={dateInput} onChange={(e) => setDateInput(e.target.value)} placeholder='YYYY-MM-DD' />
-        <button onClick={handleDateReset} className='ml-2 btn btn-danger'>Reset</button>
+        <span className="input-group-text">Start Date</span>
+        <input type="text" className="form-control" value={StartDateInput} onChange={(e) => setStartDateInput(e.target.value)} placeholder='YYYY-MM-DD' />
+        <button onClick={handleStartDateReset} className='ml-2 btn btn-danger'>Reset</button>
+      </div>
+      <div className="input-group px-5 mt-3">
+        <span className='input-group-text'>End Date</span>
+        <input type="text" className="form-control" value={endDateInput} onChange={(e) => setEndDateInput(e.target.value)} placeholder='YYYY-MM-DD' />
+        <button onClick={handleEndDateReset} className='ml-2 btn btn-danger'>Reset</button>
       </div>
     </div>
   )
