7c9b5d6bfae208c3835e04c63df586ba8aaa4845
Nathan karns <107375013+Karns11@users.noreply.github.com>
2023-05-24 21:03:17 -0400

    Update PlayerInput.js

--- src/components/PlayerInput.js
+++ src/components/PlayerInput.js
@@ -6,7 +6,7 @@ function PlayerInput({ input, setInput }) {
     <div className='column my-2'>
       <div className='input-group px-5 mb-1'>
         <span className='input-group-text'>Player</span>
-        <input type='text' className='form-control' value={input} onChange={(e) => setInput(e.target.value)} placeholder='Lebron James' />
+        <input type='text' className='form-control' value={input} onChange={(e) => setInput(e.target.value)} placeholder='Enter a player...' />
         <button onClick={() => setInput("")} className='ml-2 btn btn-danger'>Clear</button>
       </div>
     </div>
